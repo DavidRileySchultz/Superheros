5368de8e3356792acf49ff86cc30ffbe2af4e5c5
DavidRileySchultz <rileyschultz@gmail.com>
2018-09-27 14:39:40 -0500

    Create method works to add new superheros

--- Superheros/Controllers/SuperheroController.cs
+++ Superheros/Controllers/SuperheroController.cs
@@ -13,12 +13,24 @@ namespace Superheros.Controllers
         // GET: Superhero
         public ActionResult Index()
         {
-            return View();
+            return View(db.Superheroes.ToList());
         }
-
         public ActionResult Create()
         {
-
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,AlterEgo,PrimaryAbility,SecondaryAbility,CatchPhrase")] Superhero superhero)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Superheroes.Add(superhero);
+                db.SaveChanges();
+                return RedirectToAction("index");
+            }
+            ViewBag.Name = new SelectList(db.Superheroes, "ID", "Name", superhero.Name);
+            return View(superhero);
         }
         public ActionResult Details(int id)
         {
@@ -31,5 +43,9 @@ namespace Superheros.Controllers
             var heros = db.Superheroes.Where(h => h.ID == id).FirstOrDefault();
             return View(heros);
         }
+        //public ActionResult Delete(int id)
+        //{
+
+        //}
     }
 }
