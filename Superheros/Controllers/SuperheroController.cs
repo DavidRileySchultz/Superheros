565ce21cbb2100b92cec282a33d7c0c68d418254
DavidRileySchultz <rileyschultz@gmail.com>
2018-09-27 14:04:02 -0500

    Worked out Edit and Details in controller

--- Superheros/Controllers/SuperheroController.cs
+++ Superheros/Controllers/SuperheroController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Superheros.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,27 @@ namespace Superheros.Controllers
 {
     public class SuperheroController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
         // GET: Superhero
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Create()
+        {
+
+        }
+        public ActionResult Details(int id)
+        {
+            var heros = db.Superheroes.Where(h => h.ID == id).FirstOrDefault();
+            return View(heros);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var heros = db.Superheroes.Where(h => h.ID == id).FirstOrDefault();
+            return View(heros);
         }
     }
 }
