c6e57a272ae433010700adbb560dcedac565b56a
DavidRileySchultz <rileyschultz@gmail.com>
2018-09-28 11:46:11 -0500

    spaced out words in details and index views

--- Superheros/Models/Superhero.cs
+++ Superheros/Models/Superhero.cs
@@ -10,15 +10,14 @@ namespace Superheros.Models
     {
         [Key]
         public int ID { get; set; }
-
         public string Name { get; set; }
-
+        [Display(Name="Alter Ego")]
         public string AlterEgo { get; set; }
-
-        public string PrimaryAbility { get; set; }
-
+        [Display(Name="Primary Ability")]
+        public string PrimaryAbility { get; set; }        
+        [Display(Name="Secondary Ability")]
         public string SecondaryAbility { get; set; }
-
+        [Display(Name="Catch Phrase")]
         public string CatchPhrase { get; set; }
     }
 }
